c2334e01fbfe8dcdd4f6880526239bd37ad68383
leeya018 <leeyahav018@gmail.com>
2017-04-18 10:24:56 +0300

    Revert "Revert "add files to git""
    This reverts commit 9988e3ba5414801ab0bcba81a1a82511cd8dc3ba.

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { RouterModule , Routes} from '@angular/router';
+
+import { AppComponent } from './app.compnent';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes',     component: HeroesComponent }
+];
+
+
+@NgModule({
+
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    RouterModule.forRoot(routes)  
+  ],
+  exports: [ RouterModule ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
