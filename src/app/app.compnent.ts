a8c2b40fc2f950f315d737c6f45a2e76eaf4ba4e
leeya018 <leeyahav018@gmail.com>
2017-04-18 09:38:50 +0300

    add files to git

---
+++ src/app/app.compnent.ts
@@ -0,0 +1,17 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template:
+  `
+  <h1>{{title}}</h1>
+   <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+  `
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
